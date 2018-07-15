2797c7e5659036288edd11bb80ee7e291fb39e48
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2018-07-15 12:39:03 +0200

    bug fixing

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,7 +4,7 @@ import getRandomInt from '../utils';
 const description = 'Answer "yes" if number even otherwise answer "no"';
 const numberRange = 10000;
 
-export const isEven = num => num % 2 === 0;
+const isEven = num => num % 2 === 0;
 
 // return new question and right answer
 const getQuestionAndAnswer = () => {
